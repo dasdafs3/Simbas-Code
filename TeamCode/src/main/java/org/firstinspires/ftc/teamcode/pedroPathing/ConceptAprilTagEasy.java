55eb7592af0bf860132d38424328cab57ae7e3f0
HafzaO <hafzaosmankhan1234@gmail.com>
2025-12-18 15:45:03 -0500

    Make the code better

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/ConceptAprilTagEasy.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/ConceptAprilTagEasy.java
@@ -28,7 +28,6 @@
  */
 
 package org.firstinspires.ftc.teamcode.pedroPathing;
-
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -37,7 +36,6 @@ import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
-
 import java.util.List;
 
 /*
