ee434deaec308d9df722f8e0f6aa86ba948d3694
HafzaO <hafzaosmankhan1234@gmail.com>
2025-12-26 09:21:52 -0500

    Update ConceptAprilTagEasy.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/ConceptAprilTagEasy.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/ConceptAprilTagEasy.java
@@ -37,6 +37,8 @@ import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
 import java.util.List;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
 
 /*
  * This OpMode illustrates the basics of AprilTag recognition and pose estimation, using
@@ -142,26 +144,16 @@ public class ConceptAprilTagEasy extends LinearOpMode {
         for (AprilTagDetection detection : currentDetections) {
             if (detection.metadata != null) {
                 Telemetry.Line line;
-                line = telemetry.addLine(String.format("\n==== (ID %d) %s",
-                        detection.id,
-                        detection.metadata.name));
+                line = telemetry.addLine(String.format("\n==== (ID %d) %s", detection.id, detection.metadata.name));
 
-                telemetry.addLine(String.format("XYZ %6.1f %6.1f %6.1f  (inch)",
-                        detection.ftcPose.x,
-                        detection.ftcPose.y,
-                        detection.ftcPose.z));
+                telemetry.addLine(String.format("XYZ %6.1f %6.1f %6.1f  (inch)", detection.ftcPose.x, detection.ftcPose.y, detection.ftcPose.z));
 
-                telemetry.addLine(String.format("PRY %6.1f %6.1f %6.1f  (deg)",
-                        detection.ftcPose.pitch,
-                        detection.ftcPose.roll,
-                        detection.ftcPose.yaw));
+                telemetry.addLine(String.format("PRY %6.1f %6.1f %6.1f  (deg)", detection.ftcPose.pitch, detection.ftcPose.roll, detection.ftcPose.yaw));
 
-                telemetry.addLine(String.format("RBE %6.1f %6.1f %6.1f  (inch, deg, deg)",
-                        detection.ftcPose.range,
-                        detection.ftcPose.bearing,
-                        detection.ftcPose.elevation));
+                telemetry.addLine(String.format("RBE %6.1f %6.1f %6.1f  (inch, deg, deg)", detection.ftcPose.range, detection.ftcPose.bearing, detection.ftcPose.elevation));
             } else {
                 telemetry.addLine(String.format("\n==== (ID %d) Unknown", detection.id));
+
                 telemetry.addLine(String.format("Center %6.0f %6.0f   (pixels)", detection.center.x, detection.center.y));
             }
         }   // end for() loop
